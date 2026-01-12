4fbeae04b7a5a151e2c6f348fe06061c4d38641e
Fagner <fagnernezim@gmail.com>
2026-01-12 01:08:26 -0300

    login

--- src/components/AuthCallback.tsx
+++ src/components/AuthCallback.tsx
@@ -7,21 +7,31 @@ export default function AuthCallback() {
 
     useEffect(() => {
 
-        const handleCallback = async () => {
-            // troca o código da URL por sessão
-            const { error } = await supabase.auth.exchangeCodeForSession(window.location.href);
+        const url = new URL(window.location.href);
+        const code = url.searchParams.get("code");
+        const codeVerifier = localStorage.getItem('supabase.auth.code_verifier');
 
+        if (!code || !codeVerifier) {
+            throw new Error('auth code and code verifier are required')
+        }
+        console.log("Code:", url.searchParams.get("code"));
+        console.log("Code Verifier:", localStorage.getItem("supabase.auth.code_verifier"));
+
+
+        const handleCallback = async () => {
+
+            // Tenta trocar código por sessão (fluxo de e-mail/senha)
+            const { error } = await supabase.auth.exchangeCodeForSession(window.location.href);
             if (error) {
                 console.error("Erro ao confirmar:", error.message);
-                navigate("/login"); // volta pro login se falhar
+                navigate("/login");
                 return;
             }
 
-            // não precisa checar data.session aqui
-            // o AuthProvider vai atualizar user automaticamente
+            // O AuthProvider vai atualizar user automaticamente
             navigate("/dashboard");
 
-        };
+        }
 
         handleCallback();
     }, [navigate]);
