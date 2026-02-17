5ec92a029ed1dd2f6711cef78ec786743c460e35
Fagner Noe <96452626+FagnerNoe@users.noreply.github.com>
2026-02-17 01:23:37 -0300

    Increase margin top of Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ export function Home() {
     const { cores } = useStyle();
 
     return (
-        <div className="mt-5">
+        <div className="mt-10">
             <h1 className={`font-['Style_Script',cursive] text-4xl bg-clip-text ${cores.gradientBlue} text-transparent`}>
                 Associação Candidomotense de Apoio a Pessoas com Transtorno do Espectro Autista
             </h1>
