b39660194472e85a9f239e41585ac7a9f01d0041
Fagner <fagnernezim@gmail.com>
2026-05-01 17:27:53 -0300

    datas

--- src/components/Agenda.tsx
+++ src/components/Agenda.tsx
@@ -1,2 +1,27 @@
 
 
+
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+
+export function Agenda() {
+    const handleDateClick = (info: { dateStr: string; }) => {
+        alert("Você clicou em: " + info.dateStr);
+    };
+
+    return (
+        <FullCalendar
+            plugins={[dayGridPlugin, interactionPlugin]}
+            initialView="dayGridMonth"
+            dateClick={handleDateClick}
+            events={[
+                { title: "Consulta João", date: "2026-05-05" },
+                { title: "Consulta Maria", date: "2026-05-07" }
+            ]}
+        />
+    );
+}
+
+
+
